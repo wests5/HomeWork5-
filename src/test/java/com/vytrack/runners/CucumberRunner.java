99f11f37e98f64e0f573665c3e1e846cc55dc81e
nikitakharchenko95 <nikitakharchenko95@gmail.com>
2020-04-15 14:55:16 -0500

    asdf

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(   features = "src/test/resources/features",
                     glue = "com/vytrack/step_definitions",
-                    dryRun = false, tags = "@TC12" ,
+                    dryRun = false,
                     plugin = {"html:target/default-cucumber-report" , "json:target/cucumber.json",
                                  "rerun:target/rerun.txt"   }
                                     )
